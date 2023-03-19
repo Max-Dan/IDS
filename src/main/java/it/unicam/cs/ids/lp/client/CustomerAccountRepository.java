5f4c72acb3ca36f7909c81ce7f8d4bb60e3b5f60
Nicol Buratti <nicol.buratti@studenti.unicam.it>
2023-03-19 18:05:05 +0100

    gestione dati personali customer
    Signed-off-by: Nicol Buratti <nicol.buratti@studenti.unicam.it>

--- src/main/java/it/unicam/cs/ids/lp/client/CustomerAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package it.unicam.cs.ids.lp.client;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerAccountRepository
-        extends JpaRepository<CustomerAccount, Customer> {
-}
