bdd26b23da4af21f852f0d02474aa8a4c4b7a33b
Max-Dan <dani.max.dm7.dm@gmail.com>
2023-03-22 20:00:08 +0100

    UserInterface test
    Signed-off-by: Max-Dan <dani.max.dm7.dm@gmail.com>

--- src/main/java/it/unicam/cs/ids/lp/client/Customer.java
+++ src/main/java/it/unicam/cs/ids/lp/client/Customer.java
@@ -7,9 +7,13 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Objects;
 import java.util.Set;
 
@@ -18,7 +22,7 @@ import java.util.Set;
 @Setter
 @ToString
 @RequiredArgsConstructor
-public class Customer implements Serializable {
+public class Customer implements Serializable, UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
@@ -31,6 +35,37 @@ public class Customer implements Serializable {
     private LocalDate registrationDate;
     @OneToMany
     private Set<CustomerCard> cards;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
