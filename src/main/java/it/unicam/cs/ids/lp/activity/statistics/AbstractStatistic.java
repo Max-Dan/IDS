376f64f7420157258a9a3026539c1da8f740184f
Nicol Buratti <nicol.buratti@studenti.unicam.it>
2023-03-29 17:22:39 +0200

    implemetata statistica numero di clienti in una carta e aggiornata struttura
    Signed-off-by: Nicol Buratti <nicol.buratti@studenti.unicam.it>

--- src/main/java/it/unicam/cs/ids/lp/activity/statistics/AbstractStatistic.java
+++ src/main/java/it/unicam/cs/ids/lp/activity/statistics/AbstractStatistic.java
@@ -1,6 +1,5 @@
 package it.unicam.cs.ids.lp.activity.statistics;
 
-import it.unicam.cs.ids.lp.activity.Activity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -15,21 +14,15 @@ import java.util.Objects;
 @Setter
 @ToString
 @RequiredArgsConstructor
-@IdClass(StatisticId.class)
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class AbstractStatistic<T> implements Statistic<T> {
 
     @Id
-    @ManyToOne
-    @JoinColumn
-    private Activity activity;
+    @GeneratedValue
+    private long id;
 
-    @Id
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
     private StatisticType type;
 
-    @Id
     private LocalDate date;
 
     private double value;
@@ -39,11 +32,11 @@ public abstract class AbstractStatistic<T> implements Statistic<T> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         AbstractStatistic<?> that = (AbstractStatistic<?>) o;
-        return Objects.equals(activity, that.activity) && type == that.type && Objects.equals(date, that.date);
+        return id == that.id;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(activity, type, date);
+        return Objects.hash(id);
     }
 }
--- src/main/java/it/unicam/cs/ids/lp/activity/statistics/StatisticMapper.java
+++ src/main/java/it/unicam/cs/ids/lp/activity/statistics/StatisticMapper.java
@@ -1,27 +1,25 @@
 package it.unicam.cs.ids.lp.activity.statistics;
 
-import it.unicam.cs.ids.lp.activity.Activity;
+import it.unicam.cs.ids.lp.activity.statistics.card.CardClientsStatistic;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.function.BiFunction;
+import java.util.function.Function;
 
 @Service
-public class StatisticMapper implements BiFunction<StatisticType, Activity, AbstractStatistic<?>> {
+public class StatisticMapper implements Function<StatisticType, AbstractStatistic<?>> {
 
     @Override
-    public AbstractStatistic<?> apply(StatisticType type, Activity activity) {
+    public AbstractStatistic<?> apply(StatisticType type) {
         AbstractStatistic<?> statistic = getTypeClass(type);
         statistic.setType(type);
         statistic.setDate(LocalDate.now());
-        statistic.setActivity(activity);
         return statistic;
     }
 
-    private AbstractStatistic<?> getTypeClass(StatisticType type) {
-//        return switch (type) {
-//            case NUOVI_CLIENTI -> new
-//        };
-        return null;
+    public AbstractStatistic<?> getTypeClass(StatisticType type) {
+        return switch (type) {
+            case CARD_CLIENTS -> new CardClientsStatistic();
+        };
     }
 }
