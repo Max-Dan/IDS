363a0b2474deb7d43155258b953298328c7582de
Nicol Buratti <nicol.buratti@studenti.unicam.it>
2023-05-27 12:00:45 +0200

    eliminato vecchio repository
    Signed-off-by: Nicol Buratti <nicol.buratti@studenti.unicam.it>

--- src/main/java/it/unicam/cs/ids/lp/client/order/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package it.unicam.cs.ids.lp.client.order;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<CustomerOrder, Long> {
-}
