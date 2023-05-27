a370309bf3782e184ce039869b9a9dd4a4f14581
Nicol Buratti <nicol.buratti@studenti.unicam.it>
2023-05-27 11:58:37 +0200

    fix CashbackRule
    Signed-off-by: Nicol Buratti <nicol.buratti@studenti.unicam.it>

--- src/main/java/it/unicam/cs/ids/lp/client/card/programs/ProgramDataRepository.java
+++ src/main/java/it/unicam/cs/ids/lp/client/card/programs/ProgramDataRepository.java
@@ -4,9 +4,10 @@ import it.unicam.cs.ids.lp.rules.Rule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface ProgramDataRepository
         extends JpaRepository<ProgramData, Long> {
-    ProgramData findByRule(Rule<?> rule);
-
+    Set<ProgramData> findByRule(Rule<?> rule);
 }
