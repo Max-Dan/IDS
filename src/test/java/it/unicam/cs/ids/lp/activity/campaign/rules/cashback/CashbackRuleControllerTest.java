786c9e92a10e2063fd8515ee2b763b042104da98
Nicol Buratti <nicol.buratti@studenti.unicam.it>
2023-05-30 22:09:00 +0200

    corretto problema api coupon
    Signed-off-by: Nicol Buratti <nicol.buratti@studenti.unicam.it>

--- src/test/java/it/unicam/cs/ids/lp/activity/campaign/rules/cashback/CashbackRuleControllerTest.java
+++ src/test/java/it/unicam/cs/ids/lp/activity/campaign/rules/cashback/CashbackRuleControllerTest.java
@@ -31,6 +31,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -75,7 +76,7 @@ public class CashbackRuleControllerTest {
     @Test
     public void setCashback() throws Exception {
         Set<Product> products = productRepository.findByActivity_Id(activity.getId());
-        CashbackRuleRequest cashbackRequest = new CashbackRuleRequest(products, 5);
+        CashbackRuleRequest cashbackRequest = new CashbackRuleRequest(products.stream().map(Product::getId).collect(Collectors.toSet()), 5);
         String ruleJson = mvc.perform(MockMvcRequestBuilders.post("/activity/" + activity.getId()
                                 + "/campaign/" + campaign.getId() + "/cashback/add")
                         .content(objectMapper.writeValueAsString(cashbackRequest))
